f604cfbba2dfee39b6e1a1b64379eaa97ea7b84c
Fredrik Hansson <fredrik.hansson@regionuppsala.se>
2017-10-23 16:06:54 +0200

    * Tar bort de extra textfilerna (med delvis felaktig info) och skapar dessa on-the-fly istället. * Döper backuperna med hostname, eftersom de läggs på samma nätverksdisk från både gamla och nya miljön

--- etc/cron.weekly/pgbackup_audit_archive.sh
+++ etc/cron.weekly/pgbackup_audit_archive.sh
@@ -13,16 +13,33 @@ export LD_LIBRARY_PATH=$POSTGRES_HOME/lib:$LD_LIBRARY_PATH
 # Loggfil
 dt=`date +%Y%m%d`
 LOG_FILE=/tmp/postgresbackup_audit_archive_$dt.log
+hostname=`hostname`
+
+backup_name=/mnt/audit/prod/sharedservices_audit_archive_${hostname}_$dt.tar
 
 # Startar postgres backup. Byt plats för backup till mountad disk. 
-pg_dump -h localhost -p 9432 -U SharedServices -w -a --inserts -t public.sas_audit_archive -t public.sas_audit_entry_archive > /mnt/audit/prod/sharedservices_audit_archive$dt.tar 2> $LOG_FILE
+pg_dump -h localhost -p 9432 -U SharedServices -w -a --inserts -t public.sas_audit_archive -t public.sas_audit_entry_archive > $backup_name 2> $LOG_FILE
 
 
 if [ $? -eq 0 ]         # Om backupen har gått bra, starta rensning av postgres tabeller.
 then
   psql -h localhost -p 9432 -U SharedServices -w -c "truncate public.sas_audit_archive cascade;"
   psql -h localhost -p 9432 -U SharedServices -w -c "truncate public.sas_audit_entry_archive;"
-  sendmail fredrik.hansson@regionuppsala.se  < /root/pgmailsuccess_audit_archive.txt
+  
+  echo "Subject: Complete - Postgresql audit_archive backup" > /tmp/pgmailsuccess_audit_archive.txt
+  echo "" >> /tmp/pgmailsuccess_audit_archive.txt
+  echo "Backup finns under $backup_name" >> /tmp/pgmailsuccess_audit_archive.txt
+  sendmail fredrik.hansson@regionuppsala.se  < /tmp/pgmailsuccess_audit_archive.txt
+  
 else  			# Om backupen har gått illa, mejla.Här borde $MAINTANERS ersätta epostadress
-  sendmail fredrik.hansson@regionuppsala.se  < /root/pgmailerror_audit_archive.txt
+
+  echo "Subject: Error - Postgresql audit_archive backup" > /tmp/pgmailerror_audit_archive.txt
+  echo "" >> /tmp/pgmailerror_audit_archive.txt
+  echo "Se felmeddelanden i $LOG_FILE" >> /tmp/pgmailerror_audit_archive.txt
+  echo "" >> /tmp/pgmailerror_audit_archive.txt
+  echo "=============================================" >> /tmp/pgmailerror_audit_archive.txt
+  echo "" >> /tmp/pgmailerror_audit_archive.txt
+  echo $LOG_FILE >> /tmp/pgmailerror_audit_archive.txt
+
+  sendmail fredrik.hansson@regionuppsala.se  < /tmp/pgmailerror_audit_archive.txt
 fi
