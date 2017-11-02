719a6c0bc2542efbf5e8c5e14ab18fcc4469a54b
Fredrik Hansson <fredrik.hansson@regionuppsala.se>
2017-11-02 11:25:00 +0100

    Anropar ett annat skript, som även kontrollerar status på noderna.

--- etc/cron.daily/checkServerStatus.sh
+++ etc/cron.daily/checkServerStatus.sh
@@ -4,4 +4,13 @@
 # Changes:      
 #
 # Print sasserver status to temporary file.
-/opt/sas/config/Lev1/sas.servers status > /tmp/SASServerStatus.txt
+
+# Kollar status enbart på huvudnoden
+# /opt/sas/config/Lev1/sas.servers status > /tmp/SASServerStatus.txt
+
+# Kollar status på huvudnoden och workernoderna
+# To do: Kolla även Office Analytics-servern
+echo "Filen skapades:" > /tmp/SASServerStatus.txt
+date >> /tmp/SASServerStatus.txt
+echo " " >> /tmp/SASServerStatus.txt
+/opt/sas/config/Lev1/status_nodes.sh >> /tmp/SASServerStatus.txt
